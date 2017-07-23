f939d3454b72b6faba1cd8ede99a8db94c30c882
nshadov <nshadov@ravencloud.net>
2017-07-23 19:34:32 +0200

    add searching items

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python
 """Operate list of tasks."""
 
-from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.fields import HashKey, RangeKey, GlobalAllIndex
 from boto.dynamodb2.table import Table
+from boto.dynamodb2.types import NUMBER
+#  from boto.dynamodb.conditions import Key, Attr
 
 from flask import Flask
 from flask.ext.autodoc import Autodoc
 from flask.ext.dynamo import Dynamo
 from task.task import Task
 
+import json
+import decimal
+
 app = Flask(__name__)
 auto = Autodoc(app)
 app.config['DYNAMO_TABLES'] = [
-    Table('tasks_master', schema=[HashKey('id')]),
-]
+    Table(
+        'tasks_master',
+        schema=[
+            HashKey('id'),
+            RangeKey('timestamp', data_type=NUMBER)
+        ],
+        global_indexes=[
+            GlobalAllIndex('secondKeyIndex', parts=[
+                HashKey('owner')
+            ])
+        ],
+        throughput={'read': 1, 'write': 1}
+    )
+    ]
 
 dynamo = Dynamo(app)
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 @app.route('/init', methods=["GET"])
@@ -25,34 +53,39 @@ def init():
     with app.app_context():
         dynamo.create_all()
 
-    resp = list()
-
+    data = {"tables": []}
     with app.app_context():
         for table_name, table in dynamo.tables.iteritems():
-            resp.append(str((table_name, table)))
+            data["tables"].append(str((table_name, table)))
 
-    return "\n".join(resp)
+    return json.dumps(data, indent=4, cls=DecimalEncoder)
 
 
-@app.route('/task/list', methods=["GET"])
 @app.route('/', methods=["GET"])
+@app.route('/task/list', methods=["GET"])
 @auto.doc()
 def index():
     """Return list of open tasks."""
-    return "Hello World!"
+    data = {"items": []}
+    length = 0
+
+    data["table_name"] = "tasks_master"
+    results = dynamo.tasks_master.scan()
+    for r in results:
+        data["items"].append(dict(r.items()))
+        length += 1
+    data["items_length"] = length
+
+    return json.dumps(data, indent=4, cls=DecimalEncoder)
 
 
 @app.route('/task/create', methods=["GET", "POST"])
 @auto.doc()
 def task_create():
     """Add new task to a list."""
-    task = Task()
-    dynamo.tasks_master.put_item(data={
-        'id': task.get_id(),
-        'first_name': 'Randall',
-        'last_name': 'Degges',
-        'email': 'r@rdegges.com',
-    })
+    task = Task("sample")
+
+    dynamo.tasks_master.put_item(data=task.as_dict())
     return "Task ID: %s" % task.get_id()
 
 
@@ -61,6 +94,35 @@ def task_create():
 def task_destroy(task_id):
     """Add new task to a list."""
     pass
+
+
+@app.route('/task/search/<string:owner>', methods=["GET"])
+@auto.doc()
+def task_search(owner):
+    """Return list of Tasks wit specified owner."""
+    data = {"items": []}
+    length = 0
+
+    data["table_name"] = "tasks_master"
+    results = dynamo.tasks_master.query(owner__eq=owner,
+                                        index='secondKeyIndex')
+    for r in results:
+        data["items"].append(dict(r.items()))
+        length += 1
+    data["items_length"] = length
+
+    return json.dumps(data, indent=4, cls=DecimalEncoder)
+
+
+@app.route('/debug', methods=["GET"])
+@auto.doc()
+def debug():
+    """Start internal debugger (console only)."""
+    import pdb
+
+    table = dynamo.tasks_master
+    pdb.set_trace()
+    return "Hello world!"
 
 
 @app.route('/help', methods=["GET"])
--- task/task.py
+++ task/task.py
@@ -1,12 +1,19 @@
+"""Represent Task to be processed."""
+
+
 class Task(object):
     """Task to be performed in backend."""
 
-    def __init__(self):
-        """Create valid empty object."""
+    def __init__(self, owner):
+        """Create valid empty Task."""
         self._set_id()
+        self._set_creation_ts()
+        self.set_owner(owner)
+        self.title = None
+        self.desc = None
 
     def _set_id(self, id=None):
-        """Set new random ID."""
+        """Set new ID."""
         if id is None:
             import uuid
             self.id = str(uuid.uuid4())
@@ -17,14 +24,61 @@ class Task(object):
         """Return ID."""
         return self.id
 
+    def _set_creation_ts(self):
+        import time
+        self.created_at = int(time.time())
+
+    def get_created_date(self):
+        """Return date when Task has been created."""
+        return self.created_at
+
     def set_title(self, title):
+        """Set Task title."""
         self.title = title
 
     def get_title(self):
+        """Return Task title."""
         return self.title
 
     def set_description(self, desc):
+        """Set Task description."""
         self.desc = desc
 
     def get_description(self):
+        """Get Task description."""
         return self.desc
+
+    def set_owner(self, owner):
+        """Set Task owner."""
+        self.owner = owner
+
+    def get_owner(self):
+        """Get Task owner."""
+        return self.owner
+
+    def as_dict(self):
+        """Return object representation as dict."""
+        return {
+            "id": self.get_id(),
+            "timestamp": self.get_created_date(),
+            "owner": self.get_owner(),
+            "title": self.get_title(),
+            "description": self.get_description()
+            }
+
+    def from_dict(self, d):
+        """Create object from dict."""
+        if "id" in d:
+            self.set_id(d["id"])
+        if "owner" in d:
+            self.set_owner(d["owner"])
+        if "timestamp" in d:
+            self.created_at = int(d["timestamp"])
+        if "title" in d:
+            self.set_title(d["title"])
+        if "description" in d:
+            self.set_description(d["description"])
+
+    def __str__(self):
+        """Return human readable representation of Task."""
+        return "[%s] %s" % (self.id[:4], self.title)
